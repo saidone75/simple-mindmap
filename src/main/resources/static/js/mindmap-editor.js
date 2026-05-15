310b33698adae365c52925b97f2e1bc0f225e476
saidone <saidone@saidone.org>
2026-05-15 11:33:02 +0200

    Fix replaceAll with global flag

--- src/main/resources/static/js/mindmap-editor.js
+++ src/main/resources/static/js/mindmap-editor.js
@@ -1034,8 +1034,8 @@ function truncate(text, max) {
 
 function normalizeNodeText(value) {
     const normalized = sanitizePlainText((value || "")
-        .replaceAll(/\r\n/, "\n")
-        .replaceAll(/\u00a0/, " ")
+        .replaceAll(/\r\n/g, "\n")
+        .replaceAll(/\u00a0/g, " ")
         .trim());
     return normalized.length ? normalized : "Nodo";
 }
